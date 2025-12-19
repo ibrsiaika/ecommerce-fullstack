aef401cb98f3a0d6d638a1b3bb13df286616f712
ibrsiaika <ibrsiaika@users.noreply.github.com>
2025-12-19 16:17:02 +0530

    feat: Implement DashboardLoader component for enhanced loading experience in AdminDashboard

--- frontend/src/components/Loading/index.ts
+++ frontend/src/components/Loading/index.ts
@@ -1,3 +1,3 @@
 export { default as Spinner } from './Spinner';
 export { default as LoadingFallback } from './LoadingFallback';
-export { default as PageLoader } from './PageLoader';
+export { default as PageLoader } from './PageLoader';export { default as DashboardLoader } from './DashboardLoader';
